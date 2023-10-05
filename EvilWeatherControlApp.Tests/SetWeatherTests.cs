f0b98f36fcb6e6f0fd343f5b67506085feb6ef79
henridd <henri.oo@hotmail.com>
2023-10-05 21:14:41 +0200

    Upgrade .NET version to 7

--- EvilWeatherControlApp.Tests/SetWeatherTests.cs
+++ EvilWeatherControlApp.Tests/SetWeatherTests.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Moq;
+using NewEvilWeatherControlApp;
+using System.Security.Claims;
 
 namespace EvilWeatherControlApp.Tests
 {
@@ -9,13 +14,36 @@ namespace EvilWeatherControlApp.Tests
         public async Task BodyIsNull_ShouldReturnBadRequest()
         {
             // Arrange
-            var httpContext = new DefaultHttpContext();
+            var requestData = new FakeRequestData();
+            var functions = new Functions(null);
 
             // Act
-            var result = await Functions.Run(httpContext.Request, "Mars", null);
+            var result = await functions.Run(requestData, "Mars");
 
             // Assert
             Assert.That(result, Is.TypeOf<BadRequestObjectResult>());
         }
+
+        class FakeRequestData : HttpRequestData
+        {
+            private static readonly FunctionContext Context = Mock.Of<FunctionContext>();
+
+            public FakeRequestData() : base(Context)
+            {
+            }
+
+            public override Stream Body => new MemoryStream();
+            public override HttpHeadersCollection Headers { get; }
+            public override IReadOnlyCollection<IHttpCookie> Cookies { get; }
+            public override Uri Url { get; }
+            public override IEnumerable<ClaimsIdentity> Identities { get; }
+            public override string Method { get; }
+
+            public override HttpResponseData CreateResponse()
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
+
 }
